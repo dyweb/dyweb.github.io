a75955c71752ba7361f901bc6ccff4974149c073
tq5124 <wutianqi124@gmail.com>
2014-03-03 19:23:34 +0800

    if the width of screen < 1200px then forbidden the onescroll.js

--- js/global.js
+++ js/global.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
        beforeMove: function(index) {}, // This option accepts a callback function. The function will be called before the page moves.
        afterMove: function(index) {}, // This option accepts a callback function. The function will be called after the page moves.
        loop: false, // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
-       responsiveFallback: false // You can fallback to normal page scroll by defining the width of the browser in which you want the responsive fallback to be triggered. For example, set this to 600 and whenever the browser's width is less than 600, the fallback will kick in.
+       responsiveFallback: 1200 // You can fallback to normal page scroll by defining the width of the browser in which you want the responsive fallback to be triggered. For example, set this to 600 and whenever the browser's width is less than 600, the fallback will kick in.
     });
 });
 
